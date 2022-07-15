c4249c05d164b416a6c5b3af21cea70bc74d5f81
Justin Tung <justin.tung@live.ca>
2022-07-15 09:06:44 -0400

    fix to match 12 & 16 character continuous strings

--- src/parse-ic-codes-from-file.py
+++ src/parse-ic-codes-from-file.py
@@ -1,7 +1,7 @@
 # Open a text file call "idle-codes.txt" and parse the Idle Champion codes from it and write out the codes 
 # that match the format below to another text file called "ic-codes-parsed.txt"
 # Idle Champion codes are in the format of:
-#   XXXX-XXXX-XXXX or XXXX-XXXX-XXXX-XXXX
+#   XXXX-XXXX-XXXX or XXXX-XXXX-XXXX-XXXX or XXXXXXXXXXXX or XXXXXXXXXXXXXXXX
 #  where X is a character in the range of A-Z, 0-9, or !
 #  The codes in the file are inline with other text. Each code will be inline with other text and be on their own lines.
 #  The codes in the file are not in any particular order.
@@ -22,10 +22,16 @@ def getCodes(lines):
         line = line.strip()
         # if the line is not empty
         if line:
-            # parse the line and find the code in the format XXXX-XXXX-XXXX or XXXX-XXXX-XXXX-XXXX
+            # parse the line and find the code in the format XXXXXXXXXXXX with no characters after
+            # where X is a character in the range of A-Z, 0-9, !
+            # if the code is found, add it to the list of codes    
+
+            
+
+            # parse the line and find the code in the format XXXX-XXXX-XXXX or XXXX-XXXX-XXXX-XXXX or XXXXXXXXXXXX or XXXXXXXXXXXXXXXX
             # where X is a character in the range of A-Z, 0-9, !
             # if the code is found, add it to the list of codes
-            code = re.findall(r'[A-Z0-9!]{4}-[A-Z0-9!]{4}-[A-Z0-9!]{4}|[A-Z0-9!]{4}-[A-Z0-9!]{4}-[A-Z0-9!]{4}-[A-Z0-9!]{4}', line)
+            code = re.findall(r'[A-Z0-9!]{4}-[A-Z0-9!]{4}-[A-Z0-9!]{4}|[A-Z0-9!]{4}-[A-Z0-9!]{4}-[A-Z0-9!]{4}-[A-Z0-9!]{4}|[A-Z0-9!]{12}|[A-Z0-9!]{16}', line)
             if code:
                 # append the line to the list of codes
                 codes.append(code[0])
